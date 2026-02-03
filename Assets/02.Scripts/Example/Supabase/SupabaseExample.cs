3be4ea38f864f896d167d035632c76bbcc656f59
PSH <packtty18@naver.com>
2026-02-03 16:36:03 +0900

    Unitask 적용

---
+++ Assets/02.Scripts/Example/Supabase/SupabaseExample.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupabaseExample : MonoBehaviour
+{
+    public SupabaseSettingSO SupabaseSettings = null!;
+    
+}
---
+++ Assets/02.Scripts/Example/Supabase/SupabaseSettingSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Supabase", menuName = "SO/DB/Supabase/Setting")]
+public class SupabaseSettingSO : ScriptableObject
+{
+    public string URL;
+    public string KEy;
+}
