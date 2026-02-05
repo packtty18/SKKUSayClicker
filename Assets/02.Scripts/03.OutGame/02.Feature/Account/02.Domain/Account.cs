b3f66367725780fd63b27923b3a5597ad01f492e
PSH <packtty18@naver.com>
2026-02-05 16:59:52 +0900

    Account도메인의 검증 수정

--- Assets/02.Scripts/03.OutGame/02.Feature/Account/02.Domain/Account.cs
+++ Assets/02.Scripts/03.OutGame/02.Feature/Account/02.Domain/Account.cs
@@ -9,17 +9,17 @@ public class Account
     [ShowInInspector, ReadOnly] public readonly string Email;
     //보안성에 따라 비밀번호는 검증만 시행하고 클라이언트 환경에서 저장하지 않음
     //[ShowInInspector, ReadOnly] public readonly string Password;
-    [ShowInInspector, ReadOnly] public readonly bool IsSetted = false;
     public Account(string email, string password)
     {
-        EmailSpaceSpecification emailSpec = new EmailSpaceSpecification();
-        PasswordSpaceSpecification passwordSpec = new PasswordSpaceSpecification();
+        EmailValidator emailSpec = new EmailValidator();
+        PasswordValidator passwordSpec = new PasswordValidator();
 
-        if (!emailSpec.IsSatisfiedBy(email)) throw new ArgumentException(emailSpec.ErrorMessage);
-        if (!passwordSpec.IsSatisfiedBy(password)) throw new ArgumentException(passwordSpec.ErrorMessage);
+        ValidationResult emailResult = emailSpec.Validate(email);
+        ValidationResult passwordResult = passwordSpec.Validate(password);
+
+        if (!emailResult.IsValid) throw new ArgumentException(emailResult.FirstError);
+        if (!passwordResult.IsValid) throw new ArgumentException(passwordResult.FirstError);
 
         Email = email;
-        //Password = password;
-        IsSetted = true;
     }
 }
